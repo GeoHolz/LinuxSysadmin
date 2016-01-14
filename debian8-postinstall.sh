cacb42187c72e0cec218e6bb257cc89bcfdfb4dd
GeoHolz <nourssh@gmail.com>
2016-01-14 08:16:34 +0100

    Ajout de debian8-postinstall.sh en version 0.1

---
+++ debian8-postinstall.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Mon script de post installation serveur Debian 8 aka Jessie
+#
+# Geoholz - 01/2016
+# GPL
+#
+# Syntaxe: # su - -c "./jessiepostinstall.sh"
+# Syntaxe: or # sudo ./jessiepostinstall.sh
+VERSION="0.1"
+
+#=============================================================================
+# Liste des applications ànstaller: A adapter a vos besoins
+LISTE="fail2ban vim zsh curl"
+#=============================================================================
+
+# Test que le script est lance en root
+if [ $EUID -ne 0 ]; then
+  echo -e "\nLe script doit êe lancén root: # sudo $0" 1>&2
+  exit 1
+fi
+
+
+# Mise a jour de la liste des depots
+#-----------------------------------
+
+# Update 
+echo -e "\n### Mise a jour de la liste des depots\n"
+apt update
+
+# Upgrade
+echo -e "\n### Mise a jour du systeme\n"
+apt -y upgrade
+
+# Installation
+echo -e "\n### Installation des logiciels suivants: $LISTE\n"
+apt -y install $LISTE
+
+# Configuration
+#--------------
+
+echo -e "\n### Recuperation des fichiers de configuration sur http://formation-debian.via.ecp.fr/shell.html \n"
+cd /tmp
+wget http://formation-debian.via.ecp.fr/fichiers-config.tar.gz
+tar xzf fichiers-config.tar.gz
+rm fichiers-config.tar.gz
+cd /tmp/fichiers-config
+
+echo -e "\n### Configuration de ZSH et VIM \n"
+# ZSH
+cd /tmp/fichiers-config
+mv zshrc zshenv zlogin zlogout /etc/zsh/
+mv dir_colors /etc/
+curl https://raw.githubusercontent.com/GeoHolz/Extract/master/extract.sh >> /etc/zsh/zshrc
+sed -i 's/bash/zsh/g' /etc/passwd
+
+
+# VIM
+cp /tmp/fichiers-config/vimrc /etc/vim/
+
+
+echo -e -n "\n### Entrez l'adresse mail pour les rapports de securite: "
+read MAIL 
+
+# fail2ban
+sed -i 's/destemail = root@localhost/destemail = '$MAIL'/g' /etc/fail2ban/jail.conf
+
+
+
+echo -e "\n### Configuration finit. Il faut vous dénnecter/rconnecter pour que les modifications prennents effets"
+echo "### Lors de la premié connexion, il est prérable de choisir l'option 2 pour ZSH."
+# Fin du script
